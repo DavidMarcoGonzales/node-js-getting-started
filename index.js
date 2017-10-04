27e150027f528f4420959f670649b89663728179
David M. Gonzales <david_m_gonzales@hotmail.com>
2017-10-04 01:41:30 -0600

    serving static files

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 'use strict';
 
 const Hapi = require('hapi');
-
+const Path = require('path');
 const server = new Hapi.Server();
 
 server.connection({port: process.env.PORT || 5000});
 
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, reply) => {
-    reply({message: 'Hey there'});
+server.register(
+  {
+    register: require('inert')
+  }, (err) =>{
+    if(err) {throw err;}
+
+    server.route({
+      method: 'GET',
+      path: '/hello',
+      handler: (request, reply) => {
+        reply({message: 'Hey there'});
+      }
+    })
+    server.route({
+      method: 'GET',
+      path: '/{path*}',
+      handler: {
+        directory: {
+          path: Path.join(__dirname, 'build'),
+          listing: false,
+          index: true
+        }
+      }
+    })
+    server.start(err => {
+      if(err) throw err;
+      console.log(`Server listening on port ${server.info.uri}`);
+    });
   }
-})
+)
 
-server.start(err => {
-  if(err) throw err;
-  console.log(`Server listening on port ${server.info.uri}`);
-});
 
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use(express.static(__dirname + '/public'));
-
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
